99382c781c0b46fa273a135b6ceeb969498b720e
minimartzz <martin.ho.zy97@gmail.com>
2026-06-08 22:31:52 +0800

    [FIX] Recent sessions removed game icon on mobile view

--- components/tribes/RecentSessions.tsx
+++ components/tribes/RecentSessions.tsx
@@ -98,7 +98,7 @@ const GameImageCell: React.FC<{
 
   if (!imageUrl || imageError) {
     return (
-      <div className="flex items-center justify-center w-10 h-10 rounded-md bg-primary/10 shrink-0">
+      <div className="hidden sm:flex items-center justify-center w-10 h-10 rounded-md bg-primary/10 shrink-0">
         <Dices className="w-5 h-5 text-primary" />
       </div>
     );
